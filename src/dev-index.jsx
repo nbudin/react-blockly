4a250b24c8ed85b0283bc093a6d46a09d143ad64
ray_v101 <rajugautam45@gmail.com>
2019-08-30 23:10:24 +0530

    removed { alert }

--- src/dev-index.jsx
+++ src/dev-index.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Blockly, { alert } from 'blockly';
+import Blockly from 'blockly';
 
 import ReactBlocklyComponent from './index';
 import ConfigFiles from './initContent/content';
@@ -25,54 +25,4 @@ class TestEditor extends React.Component {
             blocks: [
               { type: 'text' },
               {
-                type: 'text_print',
-                values: {
-                  TEXT: {
-                    type: 'text',
-                    shadow: true,
-                    fields: {
-                      TEXT: 'abc',
-                    },
-                  },
-                },
-              },
-            ],
-          },
-        ]),
-      });
-    }, 2000);
-  }
-
-  workspaceDidChange = (workspace) => {
-    workspace.registerButtonCallback('myFirstButtonPressed', () => {
-      alert('button is pressed');
-    });
-    const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-    document.getElementById('generated-xml').innerText = newXml;
-
-    const code = Blockly.JavaScript.workspaceToCode(workspace);
-    document.getElementById('code').value = code;
-  }
-
-  render = () => (
-    <ReactBlocklyComponent.BlocklyEditor
-      toolboxCategories={this.state.toolboxCategories}
-      workspaceConfiguration={{
-        grid: {
-          spacing: 20,
-          length: 3,
-          colour: '#ccc',
-          snap: true,
-        },
-      }}
-      initialXml={ConfigFiles.INITIAL_XML}
-      wrapperDivClassName="fill-height"
-      workspaceDidChange={this.workspaceDidChange}
-    />
-  )
-}
-
-window.addEventListener('load', () => {
-  const editor = React.createElement(TestEditor);
-  ReactDOM.render(editor, document.getElementById('blockly'));
-});
+                type: 'text_prin
