7d9060a742b3695eed3dc0fa93f7170e45be6f75
Raju Gautam <rajugautam45@gmail.com>
2019-08-30 23:17:53 +0530

    button register callback added

--- src/dev-index.jsx
+++ src/dev-index.jsx
@@ -25,4 +25,54 @@ class TestEditor extends React.Component {
             blocks: [
               { type: 'text' },
               {
-                type: 'text_prin
+                type: 'text_print',
+                values: {
+                  TEXT: {
+                    type: 'text',
+                    shadow: true,
+                    fields: {
+                      TEXT: 'abc',
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        ]),
+      });
+    }, 2000);
+  }
+
+  workspaceDidChange = (workspace) => {
+    workspace.registerButtonCallback('myFirstButtonPressed', () => {
+      alert('button is pressed');
+    });
+    const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+    document.getElementById('generated-xml').innerText = newXml;
+
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+    document.getElementById('code').value = code;
+  }
+
+  render = () => (
+    <ReactBlocklyComponent.BlocklyEditor
+      toolboxCategories={this.state.toolboxCategories}
+      workspaceConfiguration={{
+        grid: {
+          spacing: 20,
+          length: 3,
+          colour: '#ccc',
+          snap: true,
+        },
+      }}
+      initialXml={ConfigFiles.INITIAL_XML}
+      wrapperDivClassName="fill-height"
+      workspaceDidChange={this.workspaceDidChange}
+    />
+  )
+}
+
+window.addEventListener('load', () => {
+  const editor = React.createElement(TestEditor);
+  ReactDOM.render(editor, document.getElementById('blockly'));
+});
