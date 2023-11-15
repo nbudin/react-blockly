4e4f457d8934f5384269f927b98ba0109a0e6381
SE <se@develup.pro>
2023-11-16 01:13:21 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,6 @@ module.exports = {
     "no-plusplus": 0,
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
