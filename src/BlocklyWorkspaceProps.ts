a4797eb59bd2776a1b89aff9b076e320a7a51268
SE <se@develup.pro>
2023-11-15 18:05:47 +0200

    fix UseBlocklyEditorProps

--- src/BlocklyWorkspaceProps.ts
+++ src/BlocklyWorkspaceProps.ts
@@ -30,15 +30,15 @@ export interface BlocklyEditorProps {
   forceData?: any;
 }
 export interface UseBlocklyEditorProps extends CommonBlocklyProps {
+  workspaceConfiguration: Blockly.BlocklyOptions;
+  toolboxConfiguration: ToolboxDefinition;
   initialXml?: string;
   initialJson?: object;
-  toolboxConfiguration?: ToolboxDefinition;
-  workspaceConfiguration: Blockly.BlocklyOptions;
   onWorkspaceChange?: (workspace: WorkspaceSvg) => void;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onError?: (error: any) => void;
-  onInject?: (newWorkspace: WorkspaceSvg) => void;
+  onInject?: (workspace: WorkspaceSvg) => void;
   onDispose?: (workspace: WorkspaceSvg) => void;
   onXmlChange?: (xml: string) => void;
-  onJsonChange?: (workspaceJson: object) => void;
+  onJsonChange?: (json: object) => void;
 }
